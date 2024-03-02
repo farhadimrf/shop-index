fb76e0e6ae804476d34417d86423143531ece5b4
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-03-02 16:13:16 +0330

    feat: implementing billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
-   return <div>HomePage</div>;
+import getBillboards from "@/actions/get-billboards";
+import Billboard from "../components/Billboard";
+import Container from "../components/ui/container";
+
+const HomePage = async () => {
+   const billboards = await getBillboards("ec20ee7a-b1cd-4577-b945-bfccbf9c900a");
+
+   return (
+      <Container>
+         <div className="space-y-10 pb-10">
+            <Billboard data={billboards} />
+         </div>
+      </Container>
+   );
 };
 
 export default HomePage;
